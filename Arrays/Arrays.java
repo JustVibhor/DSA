58c4fdb41ead9c4fabc3a90a98bf1370d8fe479e
Vibhor <vibhorebhatneriya@gmail.com>
2023-08-05 13:59:08 +0530

    Update Arrays.java

--- Arrays/Arrays.java
+++ Arrays/Arrays.java
@@ -16,4 +16,38 @@ public class Arrays {
     }
 
 
+    // brute force solution
+    public void setZeroes(int[][] matrix) {
+        int m=matrix.length, n=matrix[0].length;
+
+        List<Integer> row = new ArrayList<>();
+        List<Integer> col = new ArrayList<>();
+
+        for(int i=0; i<m; i++) {
+            for(int j=0; j<n; j++) {
+                if(matrix[i][j] == 0) {
+                    row.add(i);
+                    col.add(j);
+                }
+            }
+        }
+
+        for(int i=0; i<row.size(); i++) {
+            int index = row.get(i);
+
+            for(int j=0; j<n; j++) {
+                matrix[index][j] = 0;
+            }
+        }
+
+        for(int j=0; j<col.size(); j++) {
+            int index = col.get(j);
+            for(int i=0; i<m; i++) {
+                matrix[i][index] = 0;
+            }
+        }
+        
+    
+
+
 }
