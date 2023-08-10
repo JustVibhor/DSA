72460a6efc0239a0ee4dc6bdf738a3526c4f700a
JustVibhor <vibhorebhatneriya@gmail.com>
2023-08-10 14:05:45 +0530

    Ceiling of a number

--- BinarySearch/BinarySearch.java
+++ BinarySearch/BinarySearch.java
@@ -5,10 +5,10 @@ public class BinarySearch {
     public static void main(String[] args) {
         int[] nums = {1, 3, 5, 9, 11, 12, 18, 22, 34};
 
-        System.out.println(binarySearch(nums, 5));
+        System.out.println(normalBinarySearch(nums, 5));
     }
 
-    static int binarySearch(int[] arr, int target) {
+    static int normalBinarySearch(int[] arr, int target) {
         int start = 0;
         int end = arr.length - 1;
 
@@ -23,6 +23,49 @@ public class BinarySearch {
         }
 
         return -1;
+    }
+
+    // order-agnostic binary search
+    static int orderAgnosticBinarySearch(int[] arr, int target) {
+        int start = 0;
+        int end = arr.length - 1;
+
+        if(arr[end] > arr[start]) {
+            return normalBinarySearch(arr, target);
+        } else {
+            while(start <= end) {
+                int mid = start + (end - start)/2;
+
+                if(arr[mid] > target) {
+                    end = mid - 1;
+                } else if(arr[mid] < target) {
+                    start = mid + 1;
+                } else {
+                    return mid;
+                }
+            }
+            return -1;
+        }
+    }
+
+    // 35. Search Insert Position
+    static int searchInsert(int[] arr, int target) {
+        int start = 0;
+        int end = arr.length - 1;
+
+        while(start <= end) {
+            int mid = start + (end - start)/2;
+
+            if(arr[mid] < target) {
+                start = mid + 1;
+            } else if(arr[mid] > target) {
+                end = mid - 1;
+            } else {
+                return mid;
+            }
+        }
+
+        return start;
     }
 
 }
