fa93e626b5f0f8f95f92a9bb290bbf212cac8c7b
JustVibhor <vibhorebhatneriya@gmail.com>
2023-08-10 15:35:24 +0530

    nice

--- BinarySearch/BinarySearch.java
+++ BinarySearch/BinarySearch.java
@@ -3,9 +3,10 @@ package BinarySearch;
 public class BinarySearch {
 
     public static void main(String[] args) {
-        int[] nums = {1, 3, 5, 9, 11, 12, 18, 22, 34};
-
-        System.out.println(normalBinarySearch(nums, 5));
+//        int[] nums = {1, 3, 5, 9, 11, 12, 18, 22, 34};
+        char[] letters = {'c', 'f', 'j'};
+//        System.out.println(normalBinarySearch(nums, 5));
+        System.out.println(nextGreatestLetter(letters, 'a'));
     }
 
     static int normalBinarySearch(int[] arr, int target) {
@@ -66,6 +67,26 @@ public class BinarySearch {
         }
 
         return start;
+    }
+
+
+    static char nextGreatestLetter(char[] letters, char target) {
+        int start = 0;
+        int end = letters.length - 1;
+
+        while(start <= end) {
+            int mid = start + (end - start)/2;
+
+            if(letters[mid] < target) {
+                start = mid + 1;
+            } else if(letters[mid] > target) {cd
+                end = mid - 1;
+            } else {
+                return letters[mid];
+            }
+        }
+
+        return letters[start];
     }
 
 }
