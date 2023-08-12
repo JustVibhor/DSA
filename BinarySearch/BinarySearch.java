4a29b650ff5b78dc439bd01e4bdb22b804c94968
JustVibhor <vibhorebhatneriya@gmail.com>
2023-08-12 12:40:35 +0530

    range

--- BinarySearch/BinarySearch.java
+++ BinarySearch/BinarySearch.java
@@ -79,7 +79,7 @@ public class BinarySearch {
 
             if(letters[mid] < target) {
                 start = mid + 1;
-            } else if(letters[mid] > target) {cd
+            } else if(letters[mid] > target) {
                 end = mid - 1;
             } else {
                 return letters[mid];
@@ -87,6 +87,43 @@ public class BinarySearch {
         }
 
         return letters[start];
+    }
+
+    // 34. Find First and Last Position of Element in Sorted Array
+    public int[] searchRange(int[] nums, int target) {
+
+        int[] ans = {-1, -1};
+
+        ans[0] = binarySearchForSearchRange(nums, target, true);
+        ans[1] = binarySearchForSearchRange(nums, target, false);
+
+
+        return ans;
+    }
+
+    int binarySearchForSearchRange(int[] nums, int target, boolean isFirst) {
+        int ans = -1;
+        int start = 0;
+        int end = nums.length - 1;
+
+        while(start <= end) {
+            int mid = start + (end - start)/2;
+
+            if(nums[mid] < target) {
+                start = mid + 1;
+            } else if(nums[mid] > target) {
+                end = mid - 1;
+            } else {
+                ans = mid;
+                if(isFirst) {
+                    end = mid - 1;
+                } else {
+                    start = mid + 1;
+                }
+            }
+        }
+
+        return ans;
     }
 
 }
