d4c6823e0233a79e607344b908036f15c052884f
JustVibhor <vibhorebhatneriya@gmail.com>
2023-08-12 15:53:51 +0530

    peak

--- BinarySearch/BinarySearch.java
+++ BinarySearch/BinarySearch.java
@@ -48,6 +48,23 @@ public class BinarySearch {
         }
     }
 
+    private static int normalBinarySearch(int[] arr, int target) {
+        int start = 0;
+        int end = arr.length - 1;
+
+        while(start <= end) {
+            int mid = start + (end - start)/2;
+
+            if(arr[mid] > target) {
+                end = mid - 1;
+            } else if (arr[mid] < target) {
+                start = mid + 1;
+            } else return mid;
+        }
+
+        return -1;
+    }
+
     // 35. Search Insert Position
     static int searchInsert(int[] arr, int target) {
         int start = 0;
@@ -138,6 +155,24 @@ public class BinarySearch {
         }
 
         return normalBinarySearch(nums, target, start, end);
+    }
+
+    // 852. Peak Index in a Mountain Array
+    static int peakIndexInMountainArray(int[] arr) {
+        int start = 0;
+        int end = arr.length - 1;
+
+        while(start < end) {
+            int mid = start + (end - start) / 2;
+
+            if(arr[mid] > arr[mid+1]) {
+                end = mid;
+            } else {
+                start = mid + 1;
+            }
+        }
+
+        return start;
     }
 
 }
