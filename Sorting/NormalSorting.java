f17d5cfa01fdd3003806ecb30ff1c10159b81e46
JustVibhor <vibhorebhatneriya@gmail.com>
2023-09-14 16:35:27 +0530

    Add

--- Sorting/NormalSorting.java
+++ Sorting/NormalSorting.java
@@ -51,7 +51,7 @@ public class NormalSorting {
 
     // sort in pieces
     static void insertionSort(int[] nums) {
-        for(int i=0; i<nums.length-1; i++) {
+        for(int i=0; i<nums.length - 1; i++) {
             for(int j=i+1; j>0; j--) {
                 if(nums[j] < nums[j-1]) {
                     swap(nums, j, j-1);
