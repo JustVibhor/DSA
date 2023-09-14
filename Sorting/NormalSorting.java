5005c08de6c6a35968bd7def9337f2767e758cd5
JustVibhor <vibhorebhatneriya@gmail.com>
2023-09-14 13:04:27 +0530

    ought to be

--- Sorting/NormalSorting.java
+++ Sorting/NormalSorting.java
@@ -21,8 +21,10 @@ public class NormalSorting {
     static void bubbleSort(int[] nums) {
         boolean swapped;
 
+        // outer loop will run till n-1
         for(int i=0; i<nums.length; i++) {
             swapped = false;
+            // inner loop will run for n - i as swapped element will be at last and will be the biggest
             for(int j=1; j<nums.length - i; j++) {
                 if(nums[j] > nums[j-1]) {
                     swap(nums, j, j-1);
