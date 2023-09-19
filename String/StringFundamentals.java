6461792afe307fd1096b3b7d3d316ee6f448ce13
JustVibhor <vibhorebhatneriya@gmail.com>
2023-09-19 20:33:31 +0530

    String Builder

--- String/StringFundamentals.java
+++ String/StringFundamentals.java
@@ -7,16 +7,15 @@ public class StringFundamentals {
         System.out.println((char)('b' + 2)); // gives char value of 100 i.e 'd'
         System.out.println("a" + 1); // when int is concatenates with String it calls Integer instead of int
 
-        // String Performance Issue
-        // problem with this approach is
-        // everytime series += ch happens a new String object is created which reduces its perfomance
-        // So n loop Tc comes out to be O(n**2)
-        String series = "";
+        // To Avoid the problem of creating new obj everytime we can use StringBuilder
+        StringBuilder series = new StringBuilder();
 
         for(int i=0; i<26; i++) {
             char ch = (char)('a' + i);
-            series += ch;
+            series.append(ch);
         }
         System.out.println(series);
     }
+
+
 }
