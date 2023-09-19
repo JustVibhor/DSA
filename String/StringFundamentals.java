1f7a0c965649ef9c5af378cbc1c1c7694c4a747c
JustVibhor <vibhorebhatneriya@gmail.com>
2023-09-19 19:06:08 +0530

    Strings

---
+++ String/StringFundamentals.java
@@ -0,0 +1,9 @@
+package String;
+
+public class StringFundamentals {
+    public static void main(String[] args) {
+        System.out.println('a' + 'b'); // gives int value i.e. adds binary value of 'a' and 'b'
+        System.out.println("a" + "b"); // concatenates a and b in a string value
+        System.out.println((char)('b' + 2)); // gives char value of 100 i.e 'd'
+    }
+}
