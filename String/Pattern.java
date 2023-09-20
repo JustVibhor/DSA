6ab929fcf20c88a669049fe002cc6ec084d671d1
JustVibhor <vibhorebhatneriya@gmail.com>
2023-09-20 17:23:04 +0530

    Patterns

--- String/Pattern.java
+++ String/Pattern.java
@@ -4,7 +4,8 @@ import java.util.Locale;
 
 public class Pattern {
     public static void main(String[] args) {
-        System.out.println(palindrome("aabaA"));
+//        System.out.println(palindrome("aabaA"));
+        pattern3(5);
     }
 
     // Palindrome
@@ -22,5 +23,52 @@ public class Pattern {
             }
         }
         return true;
+    }
+
+//   *
+//   *  *
+//   *  *  *
+//   *  *  *  *
+//   *  *  *  *  *
+    static void pattern1(int n) {
+        for(int i=0; i<n; i++) {
+            for(int j=0; j<=i; j++) {
+                System.out.print(" * ");
+            }
+            System.out.println();
+        }
+    }
+
+
+//      *  *  *  *  *
+//      *  *  *  *
+//      *  *  *
+//      *  *
+//      *
+    static void pattern2(int n) {
+        for(int i=0; i<n; i++) {
+            for(int j=n-i; j>0; j--) {
+                System.out.print(" * ");
+            }
+            System.out.println();
+        }
+    }
+
+// 1
+// 1  2
+// 1  2  3
+// 1  2  3  4
+// 1  2  3  4  5
+    static void pattern3(int n) {
+        for (int i = 1; i <= n; i++) {
+            for(int j=1; j<=i; j++) {
+                System.out.print(" " + j + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    static void patter4(int n) {
+
     }
 }
