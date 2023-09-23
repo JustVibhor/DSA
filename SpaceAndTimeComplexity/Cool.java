23a23a553cae325d1c89c505a13c78a8f91644f4
JustVibhor <vibhorebhatneriya@gmail.com>
2023-09-23 22:23:48 +0530

    Non homogeneous

--- SpaceAndTimeComplexity/Cool.java
+++ SpaceAndTimeComplexity/Cool.java
@@ -1,8 +1,12 @@
 package SpaceAndTimeComplexity;
 
+// Akra Baazi Formula for divide and conq
+// for linear use f(n) = alpha**n and u move on to find c1 and c2
+// for non homogeneous do normally just and add g(n), and guess c
+
 public class Cool {
     public static void main(String[] args) {
-        System.out.println(fibonacciInConstantTime(6));
+        System.out.println(fibonacciInConstantTime(100));
     }
 
     static int fibonacciInConstantTime(int n) {
