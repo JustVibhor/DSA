f83588985a46b82a32b310406c9a1c072bdc26ae
JustVibhor <vibhorebhatneriya@gmail.com>
2023-09-24 11:47:19 +0530

    Brute Force

--- BitManipulation/Bits.java
+++ BitManipulation/Bits.java
@@ -1,15 +1,34 @@
 package BitManipulation;
 
+import java.util.Arrays;
+
 public class Bits {
     public static void main(String[] args) {
-        int n = 12;
-        int k = 3;
-
-        if (((1 << k) & n) != 0)
-            System.out.print("Not zero");
-        else
-            System.out.print("Zero");
+        int[] nums = {2, 3, 4, 1, 2, 1, 3, 6, 4};
+        System.out.println(whichElementRepeating(nums));
     }
 
+    static String isEven(int n) {
+        return (n&1) == 0 ? n + " is a even number" : n + " is an odd number";
+    }
+
+    static int whichElementRepeating(int[] nums) {
+        int n = nums.length;
+
+        for(int i=0; i<n; i++) {
+            int j;
+
+            for(j=0; j<n; j++) {
+                if(i != j && nums[i] == nums[j]) {
+                    break;
+                }
+            }
+
+            if(j == n) {
+                return nums[i];
+            }
+        }
+        return -1;
+    }
 
 }
