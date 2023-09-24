138b17ff326e1c15e70a2f89865297c6dc84e6f6
JustVibhor <vibhorebhatneriya@gmail.com>
2023-09-24 18:58:58 +0530

    Damn that took a lot out of me

--- BitManipulation/Bits.java
+++ BitManipulation/Bits.java
@@ -4,9 +4,21 @@ import java.util.Arrays;
 
 public class Bits {
     public static void main(String[] args) {
-//        int[] nums = {2, 3, 4, 1, 2, 1, 3, 6, 4, 6, 9};
-//        System.out.println(whichElementRepeating(nums));
-        System.out.println(findIthBit(15, 2));
+        int[] nums = {2, 2, 3, 2, 7, 7, 8, 7, 8, 8};
+        int n = findNonRepeatingElement(nums);
+        System.out.println(covertBinaryToDecimal(n));
+//        System.out.println(covertDecimalToBinary(11));
+    }
+
+    private static int covertBinaryToDecimal(int n) {
+        int ans = 0;
+        int count = 0;
+        while(n > 0) {
+            ans += (int) ((n % 10)*Math.pow(2, count));
+            count++;
+            n /= 10;
+        }
+        return ans;
     }
 
     static String isEven(int n) {
@@ -23,7 +35,61 @@ public class Bits {
 
     static int findIthBit(int n, int i) {
 
-        return (n >> i-1) & 1;
+        return n | (1 << i-1);
+    }
+
+    static int resetIthBit(int n, int i) {
+        return (n >> i) << i;
+    }
+
+    static int findRightMostSetBit(int n) {
+//        if(n == 0) {
+//            return -1;
+//        }
+//        int count = 1;
+//        while(true) {
+//            if((n & 1) == 1) {
+//                return count;
+//            }
+//            n = n >> 1;
+//            count++;
+//        }
+        return n & -n;
+    }
+
+    // Repetition is in odd
+    static int findNonRepeatingElement(int[] nums) {
+        int ans = 0;
+        for(int num: nums) {
+            int n = covertDecimalToBinary(num);
+            ans += n;
+//            System.out.println(ans);
+        }
+//        return ans;
+
+        int real_ans = 0;
+        int count = 0;
+
+        while(ans > 0) {
+            real_ans += (int) (((ans%10)%3)*Math.pow(10, count));
+            count++;
+            ans /= 10;
+
+        }
+        return real_ans;
+    }
+
+    static int covertDecimalToBinary(int n) {
+        int ans = 0;
+        int count = 0;
+        while(n > 0) {
+            ans += (int) ((n % 2)*Math.pow(10, count));
+            n /= 2;
+            count++;
+        }
+
+        return ans;
+
     }
 
 }
