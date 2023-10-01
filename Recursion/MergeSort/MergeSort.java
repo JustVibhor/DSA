137f9eaf88e45b19f1e9dd650184b8367201997f
JustVibhor <vibhorebhatneriya@gmail.com>
2023-10-01 16:30:40 +0530

    Merge sort from geeks

---
+++ Recursion/MergeSort/MergeSort.java
@@ -0,0 +1,82 @@
+package Recursion.MergeSort;
+
+public class MergeSort {
+    public static void main(String[] args) {
+
+    }
+
+    /* Steps
+    * 1. Divide array into 2 parts
+    * 2. Get both parts sorted via recursion
+    * 3. Merge the sorted parts
+    * ex: nums1 = [3, 5, 9] nums2 = [4, 6, 8]
+    * create new arr = [nums1.length + nums2.length]
+    *   */
+    void merge(int arr[], int l, int m, int r)
+    {
+        // Find sizes of two subarrays to be merged
+        int n1 = m - l + 1;
+        int n2 = r - m;
+
+        // Create temp arrays
+        int L[] = new int[n1];
+        int R[] = new int[n2];
+
+        // Copy data to temp arrays
+        for (int i = 0; i < n1; ++i)
+            L[i] = arr[l + i];
+        for (int j = 0; j < n2; ++j)
+            R[j] = arr[m + 1 + j];
+
+        // Merge the temp arrays
+
+        // Initial indices of first and second subarrays
+        int i = 0, j = 0;
+
+        // Initial index of merged subarray array
+        int k = l;
+        while (i < n1 && j < n2) {
+            if (L[i] <= R[j]) {
+                arr[k] = L[i];
+                i++;
+            }
+            else {
+                arr[k] = R[j];
+                j++;
+            }
+            k++;
+        }
+
+        // Copy remaining elements of L[] if any
+        while (i < n1) {
+            arr[k] = L[i];
+            i++;
+            k++;
+        }
+
+        // Copy remaining elements of R[] if any
+        while (j < n2) {
+            arr[k] = R[j];
+            j++;
+            k++;
+        }
+    }
+
+    // Main function that sorts arr[l..r] using
+    // merge()
+    void sort(int arr[], int l, int r)
+    {
+        if (l < r) {
+
+            // Find the middle point
+            int m = l + (r - l) / 2;
+
+            // Sort first and second halves
+            sort(arr, l, m);
+            sort(arr, m + 1, r);
+
+            // Merge the sorted halves
+            merge(arr, l, m, r);
+        }
+    }
+}
