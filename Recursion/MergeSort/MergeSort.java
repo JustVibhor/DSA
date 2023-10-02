8502676861cd758d43c0ba5cdc837dc0d1f7c451
JustVibhor <vibhorebhatneriya@gmail.com>
2023-10-02 15:56:26 +0530

    Better approach

--- Recursion/MergeSort/MergeSort.java
+++ Recursion/MergeSort/MergeSort.java
@@ -1,82 +1,50 @@
 package Recursion.MergeSort;
 
+import java.util.Arrays;
+
 public class MergeSort {
     public static void main(String[] args) {
+        int[] nums = {7, 5, 2};
 
+        System.out.println(Arrays.toString(mergeSort(nums)));
     }
 
-    /* Steps
-    * 1. Divide array into 2 parts
-    * 2. Get both parts sorted via recursion
-    * 3. Merge the sorted parts
-    * ex: nums1 = [3, 5, 9] nums2 = [4, 6, 8]
-    * create new arr = [nums1.length + nums2.length]
-    *   */
-    void merge(int arr[], int l, int m, int r)
-    {
-        // Find sizes of two subarrays to be merged
-        int n1 = m - l + 1;
-        int n2 = r - m;
+   static int[] mergeSort(int[] nums) {
+        if(nums.length == 1) {
+            return nums;
+        }
 
-        // Create temp arrays
-        int L[] = new int[n1];
-        int R[] = new int[n2];
+        int mid = nums.length/2;
 
-        // Copy data to temp arrays
-        for (int i = 0; i < n1; ++i)
-            L[i] = arr[l + i];
-        for (int j = 0; j < n2; ++j)
-            R[j] = arr[m + 1 + j];
+        int[] left = mergeSort(Arrays.copyOfRange(nums, 0, mid));
+        int[] right = mergeSort(Arrays.copyOfRange(nums, mid, nums.length));
 
-        // Merge the temp arrays
+        return merge(left, right);
+   }
 
-        // Initial indices of first and second subarrays
-        int i = 0, j = 0;
+   static int[] merge(int[] left, int[] right) {
+        int[] mix = new int[left.length + right.length];
 
-        // Initial index of merged subarray array
-        int k = l;
-        while (i < n1 && j < n2) {
-            if (L[i] <= R[j]) {
-                arr[k] = L[i];
-                i++;
-            }
-            else {
-                arr[k] = R[j];
-                j++;
+        int i = 0, j = 0, k = 0;
+
+        while(i < left.length && j < right.length) {
+            if(left[i] < right[j]) {
+                mix[k] = left[i++];
+            } else {
+                mix[k] = right[j++];
             }
             k++;
         }
 
-        // Copy remaining elements of L[] if any
-        while (i < n1) {
-            arr[k] = L[i];
-            i++;
-            k++;
+        while(i < left.length) {
+            mix[k++] = left[i++];
         }
 
-        // Copy remaining elements of R[] if any
-        while (j < n2) {
-            arr[k] = R[j];
-            j++;
-            k++;
+        while(j < right.length) {
+            mix[k++] = right[j++];
         }
-    }
 
-    // Main function that sorts arr[l..r] using
-    // merge()
-    void sort(int arr[], int l, int r)
-    {
-        if (l < r) {
+        return mix;
+   }
 
-            // Find the middle point
-            int m = l + (r - l) / 2;
-
-            // Sort first and second halves
-            sort(arr, l, m);
-            sort(arr, m + 1, r);
-
-            // Merge the sorted halves
-            merge(arr, l, m, r);
-        }
-    }
 }
