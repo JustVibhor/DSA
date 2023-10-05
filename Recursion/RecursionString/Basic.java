3fce7730377c068db42224ab1c1b48a1eb61bd74
JustVibhor <vibhorebhatneriya@gmail.com>
2023-10-05 12:21:05 +0530

    Strings and Subsets

--- Recursion/RecursionString/Basic.java
+++ Recursion/RecursionString/Basic.java
@@ -1,10 +1,54 @@
 package Recursion.RecursionString;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
 public class Basic {
     public static void main(String[] args) {
 
-        System.out.println(skipApple("somethingAppleNothing"));
+        // System.out.println(skipApple("somethingAppleNothing"));
+        List<String> list = new ArrayList<>();
+        System.out.println(subSetsListBody("abc", ""));
+        ;
 
+    }
+
+
+    static void subSets(String str, String ans) {
+        if (str.isEmpty()) {
+            System.out.println(ans);
+            return;
+        }
+
+        subSets(str.substring(1), ans + str.charAt(0));
+        subSets(str.substring(1), ans);
+    }
+
+    static List<String> subSetsListArgs(String str, String ans, List<String> list) {
+        if (str.isEmpty()) {
+            list.add(Objects.equals(ans, "") ? "null" : ans);
+            return list;
+        }
+
+        subSetsListArgs(str.substring(1), ans + str.charAt(0), list);
+        subSetsListArgs(str.substring(1), ans, list);
+
+        return list;
+    }
+
+    static List<String> subSetsListBody(String str, String ans) {
+        List<String> list = new ArrayList<>();
+
+        if(str.isEmpty()) {
+            list.add(Objects.equals(ans, "") ? "null" : ans);
+            return list;
+        }
+
+        list.addAll(subSetsListBody(str.substring(1), ans+str.charAt(0)));
+        list.addAll(subSetsListBody(str.substring(1), ans));
+
+        return list;
     }
 
 
