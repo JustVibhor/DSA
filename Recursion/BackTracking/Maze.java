a971dad1501a6f209bcb6df341ba2fe007921c38
JustVibhor <vibhorebhatneriya@gmail.com>
2023-10-09 20:54:10 +0530

    BackTracking last concept question

--- Recursion/BackTracking/Maze.java
+++ Recursion/BackTracking/Maze.java
@@ -1,12 +1,13 @@
 package Recursion.BackTracking;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Maze {
     public static void main(String[] args) {
         boolean[][] maze = new boolean[3][3];
-        List<String> list = mazeAllDirection(maze, 0, 0, "");
+        List<String> list = mazeAllDirectionMoreInfo(maze, 0, 0, "", new int[3][3], 1);
         System.out.println(list);
     }
 
@@ -36,40 +37,85 @@ public class Maze {
 
     }
 
-    static List<String> mazeAllDirection(boolean[][] maze, int row, int col, String p) {
+    static List<String> mazeAllDirection(boolean[][] visited, int row, int col, String p) {
         List<String> list = new ArrayList<>();
-        if (row == maze.length - 1 && col == maze.length - 1) {
+        if (row == visited.length - 1 && col == visited.length - 1) {
             list.add(p);
 
             return list;
         }
 
 
-        if (!maze[row][col] && row > 0) {
-            maze[row][col] = true;
-            list.addAll(mazeAllDirection(maze, row - 1, col, p + "U"));
-            maze[row][col] = false;
+        if (!visited[row][col]) {
+            visited[row][col] = true;
+
+            if (row > 0) {
+                list.addAll(mazeAllDirection(visited, row - 1, col, p + "U"));
+            }
+
+            if (col > 0) {
+                list.addAll(mazeAllDirection(visited, row, col - 1, p + "L"));
+            }
+
+            if (row < visited.length - 1) {
+                list.addAll(mazeAllDirection(visited, row + 1, col, p + "D"));
+            }
+
+            if (col < visited[0].length - 1) {
+                list.addAll(mazeAllDirection(visited, row, col + 1, p + "R"));
+            }
+
+            // WHILE COMING BACK RESTORE IT TO DEFAULT
+            visited[row][col] = false;
         }
 
-        if (!maze[row][col] && col > 0) {
-            maze[row][col] = true;
-            list.addAll(mazeAllDirection(maze, row, col - 1, p + "L"));
-            maze[row][col] = false;
-        }
-
-        if (!maze[row][col] && row < maze.length - 1) {
-            maze[row][col] = true;
-            list.addAll(mazeAllDirection(maze, row + 1, col, p + "D"));
-            maze[row][col] = false;
-        }
-
-        if (!maze[row][col] && col < maze[0].length - 1) {
-            maze[row][col] = true;
-            list.addAll(mazeAllDirection(maze, row, col + 1, p + "R"));
-            maze[row][col] = false;
-        }
 
         return list;
 
     }
+
+
+    static List<String> mazeAllDirectionMoreInfo(boolean[][] visited, int row, int col, String p, int[][] visual, int count) {
+        List<String> list = new ArrayList<>();
+        if (row == visited.length - 1 && col == visited.length - 1) {
+            for(int[] arr: visual) {
+                System.out.println(Arrays.toString(arr));
+            }
+            System.out.println(p);
+            System.out.println();
+            list.add(p);
+            return list;
+        }
+
+
+        if (!visited[row][col]) {
+            visited[row][col] = true;
+            visual[row][col] = count;
+
+            if (row > 0) {
+                list.addAll(mazeAllDirectionMoreInfo(visited, row - 1, col, p + "U", visual, count + 1));
+            }
+
+            if (col > 0) {
+                list.addAll(mazeAllDirectionMoreInfo(visited, row, col - 1, p + "L", visual, count + 1));
+            }
+
+            if (row < visited.length - 1) {
+                list.addAll(mazeAllDirectionMoreInfo(visited, row + 1, col, p + "D", visual, count + 1));
+            }
+
+            if (col < visited[0].length - 1) {
+                list.addAll(mazeAllDirectionMoreInfo(visited, row, col + 1, p + "R", visual, count + 1));
+            }
+
+            visual[row][col] = 0;
+            // WHILE COMING BACK RESTORE IT TO DEFAULT
+            visited[row][col] = false;
+        }
+
+
+        return list;
+
+    }
+
 }
